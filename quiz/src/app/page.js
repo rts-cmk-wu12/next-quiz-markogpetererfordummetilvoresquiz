9eb10edb75274e261c19d69f8d241cad2e7906fb
Davidski7 <U2486889@rts-365.dk>
2025-06-25 10:34:09 +0200

    Er igang med billeder

--- quiz/src/app/page.js
+++ quiz/src/app/page.js
@@ -1,31 +1,61 @@
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 
-export default async function Home() {
-
+export default async function Home({ searchParams }) {
   const response = await fetch("https://opentdb.com/api_category.php");
   const data = await response.json();
   const categories = data.trivia_categories;
 
-  console.log(data)
+
+  const categoryImages = {
+    9: "quiz/public/unnamed.png",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+    18: "",
+    19: "",
+    20: "",
+    21: "",
+    22: "",
+    23: "",
+    24: "",
+    25: "",
+    26: "",
+    27: "",
+    28: "",
+    29: "",
+    30: "",
+    31: "",
+    32: "",
+  };
 
   return (
-    <main className=" text-center">
+    <main className="text-center p-6">
       <h1 className="text-3xl mb-6">Velkommen til vores quiz!</h1>
 
-      <h2>Tilgængelige kategorier:</h2>
+      <h2 className="text-xl mb-2">Tilgængelige kategorier:</h2>
+      <ul className="space-y-4">
+        {categories.map((categorie) => {
+          const imagePath = categoryImages[categorie.id] || "";
 
-      <ul>
-        {categories.map((categorie) => (
-          <li key={categorie.id}>
-
-            <Link
-              href={`/question/${categorie.id}`}
-            >
-              {categorie.name}
-            </Link>
-          </li>
-        ))}
+          return (
+            <li key={categorie.id} className="flex items-center justify-center">
+              <Link
+                href={{
+                  pathname: `/question/${categorie.id}`,
+                  query: { image: imagePath },
+                }}
+              >
+                {categorie.name}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
