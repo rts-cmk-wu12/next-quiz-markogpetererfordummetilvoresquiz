8e2bb5b12d8881b8369d0e9a50e0c4d7fa7800ba
Benjamin1243 <U2486886@rts-365.dk>
2025-06-25 10:36:11 +0200

    fixed fails

--- quiz/src/app/question/[id]/page.jsx
+++ quiz/src/app/question/[id]/page.jsx
@@ -1,4 +1,4 @@
-export default async function Question() {
+
 export default async function Question({params}){
 
 let actual = 0;
@@ -6,29 +6,26 @@ const id = await params
 const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${id?.id}&type=multiple`);
 
 const data = await response.json()
-console.log(data.results[actual])
-const answers = [...data?.results[actual]?.incorrect_answers,data?.results[actual]?.correct_answer ]
-console.log(answers)
 
-    return (
-        <main className="bg-[url('https://static.wixstatic.com/media/c23ee4_8eb76a5d097f4425b3801b5420707127~mv2.gif')] h-lvh  bg-[cover] px-[10vw] grid">
-            <div className="h-1 bg-red-500 mt-10"> <div className="h-1 bg-amber-300"></div></div>
-            <article className="justify-self-center bg-amber-50">
-                <h1>Quiz navn</h1>
+const answers = [...data?.results[actual]?.incorrect_answers,data?.results[actual]?.correct_answer ]
+
+
     return(
 <main className="bg-[url('https://static.wixstatic.com/media/c23ee4_8eb76a5d097f4425b3801b5420707127~mv2.gif')] grid-cols-1 grid-rows-1 h-lvh  bg-[cover] px-[10vw] flex flex-col">
     <div className="h-1 bg-red-500 mt-10"> <div className="h-1 bg-amber-300"></div></div>
         <article className=" bg-amber-50 mt-10 p-10">
-        <h1 className="text-center">{data.results[actual].question}</h1>
+        <h1 className="text-center">{data?.results[actual]?.question}</h1>
         <div className="grid grid-rows-2 grid-cols-2 pt-16 gap-11">
          {answers?.map((element) =>{
-            return <button className="border-4 border-amber-300" key={element}>{element}</button>
+            return <button onClick={()=>{
+                alert("er")
+            }} className="border-2 border-amber-300 " key={element}>{element}</button>
          })}
          </div>
 
-            </article>
+        </article>
         </main>
     )
 
-
+  
 }
