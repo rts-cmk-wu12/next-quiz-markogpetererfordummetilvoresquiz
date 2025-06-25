21c8a487865fa06360318630976bcc0ca9f605dc
Benjamin1243 <U2486886@rts-365.dk>
2025-06-25 09:46:23 +0200

    added slider and question page

---
+++ quiz/src/app/question/page.jsx
@@ -0,0 +1,11 @@
+export default function Question(){
+    return(
+<main className="bg-[url('https://static.wixstatic.com/media/c23ee4_8eb76a5d097f4425b3801b5420707127~mv2.gif')] h-lvh  bg-[cover] px-[10vw] grid">
+    <div className="h-1 bg-red-500 mt-10"> <div className="h-1 bg-amber-300"></div></div>
+        <article className="justify-self-center bg-amber-50">
+        <h1>Quiz navn</h1>
+
+        </article>
+        </main>
+    )
+}
