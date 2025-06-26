cac9abbcc6ecb1fc7355f829ca89de701d968a60
Davidski7 <U2486889@rts-365.dk>
2025-06-26 09:00:35 +0200

    Finishpage tilføjet

---
+++ quiz/src/app/finish/page.jsx
@@ -0,0 +1,8 @@
+export default function FinishPage() {
+
+    return (
+        <h1>
+            Du er klam og en kæmpe taber uden en fremtid siden du prøver vores lorte quiz
+        </h1>
+    )
+}
